0c76828ba5bfead30951b87ef7afc914e31aa9fe
11S20015 Winanda Greace A. Sinaga <winandagreaceas@gmail.com>
2023-11-28 23:37:34 +0700

    update Login

--- src/test/java/Login/Login.java
+++ src/test/java/Login/Login.java
@@ -28,39 +28,39 @@ public class Login {
         restAssuredThat(response -> response.statusCode(200));
     }
 
-    @Step("I set POST method with valid endpoint and invalid body requests Login Admin")
-    public String setInvalidLoginAdminReq1(){
-        return url;
-    }
-
-    @Step("I send Login requests to connect to APIs L2")
-    public void sendLoginAdminReq2(){
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("email", "admin@gmail.com");
-        requestBody.put("password", "123456");
-
-        SerenityRest.given().when().get(setInvalidLoginAdminReq1());
-    }
-
-    @Step("I receive HTTP response code 400 for Invalid Login Admin")
-    public void receiveLoginAdminResp2(){
-        restAssuredThat(response -> response.statusCode(400));
-    }
-
-    @Step("I set GET method with valid endpoint and body requests Login Admin")
-    public String setInvalidLoginAdminReq2(){
-        return url;
-    }
-
-    @Step("I send Login requests to connect to APIs L3")
-    public void sendLoginAdminReq3(){
-        SerenityRest.given().when().get(setInvalidLoginAdminReq2());
-    }
-
-    @Step("I receive HTTP response code 400 for Invalid Request Login Admin")
-    public void receiveLoginAdminResp3(){
-        restAssuredThat(response -> response.statusCode(400));
-    }
+//    @Step("I set POST method with valid endpoint and invalid body requests Login Admin")
+//    public String setInvalidLoginAdminReq1(){
+//        return url;
+//    }
+//
+//    @Step("I send Login requests to connect to APIs L2")
+//    public void sendLoginAdminReq2(){
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("email", "admin@gmail.com");
+//        requestBody.put("password", "123456");
+//
+//        SerenityRest.given().when().get(setInvalidLoginAdminReq1());
+//    }
+//
+//    @Step("I receive HTTP response code 400 for Invalid Login Admin")
+//    public void receiveLoginAdminResp2(){
+//        restAssuredThat(response -> response.statusCode(400));
+//    }
+//
+//    @Step("I set GET method with valid endpoint and body requests Login Admin")
+//    public String setInvalidLoginAdminReq2(){
+//        return url;
+//    }
+//
+//    @Step("I send Login requests to connect to APIs L3")
+//    public void sendLoginAdminReq3(){
+//        SerenityRest.given().when().get(setInvalidLoginAdminReq2());
+//    }
+//
+//    @Step("I receive HTTP response code 400 for Invalid Request Login Admin")
+//    public void receiveLoginAdminResp3(){
+//        restAssuredThat(response -> response.statusCode(400));
+//    }
 
     @Step("I set POST method with valid endpoint and body requests Login SuperAdmin")
     public String setValidLoginSuperAdminReq(){
@@ -81,37 +81,37 @@ public class Login {
         restAssuredThat(response -> response.statusCode(200));
     }
 
-    @Step("I set POST method with valid endpoint and invalid body requests Login SuperAdmin")
-    public String setInvalidLoginSuperAdminReq1(){
-        return url;
-    }
-
-    @Step("I send Login requests to connect to APIs L5")
-    public void sendLoginSuperAdminReq2(){
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("email", "admin@gmail.com");
-        requestBody.put("password", "123456");
-
-        SerenityRest.given().when().get(setInvalidLoginSuperAdminReq1());
-    }
-
-    @Step("I receive HTTP response code 400 for Invalid Login SuperAdmin")
-    public void receiveLoginSuperAdminResp2(){
-        restAssuredThat(response -> response.statusCode(400));
-    }
-
-    @Step("I set GET method with valid endpoint and body requests Login SuperAdmin")
-    public String setInvalidLoginSuperAdminReq2(){
-        return url;
-    }
-
-    @Step("I send Login requests to connect to APIs L6")
-    public void sendLoginSuperAdminReq3(){
-        SerenityRest.given().when().get(setInvalidLoginAdminReq2());
-    }
-
-    @Step("I receive HTTP response code 400 for Invalid Request Login SuperAdmin")
-    public void receiveLoginSuperAdminResp3(){
-        restAssuredThat(response -> response.statusCode(400));
-    }
+//    @Step("I set POST method with valid endpoint and invalid body requests Login SuperAdmin")
+//    public String setInvalidLoginSuperAdminReq1(){
+//        return url;
+//    }
+//
+//    @Step("I send Login requests to connect to APIs L5")
+//    public void sendLoginSuperAdminReq2(){
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("email", "admin@gmail.com");
+//        requestBody.put("password", "123456");
+//
+//        SerenityRest.given().when().get(setInvalidLoginSuperAdminReq1());
+//    }
+//
+//    @Step("I receive HTTP response code 400 for Invalid Login SuperAdmin")
+//    public void receiveLoginSuperAdminResp2(){
+//        restAssuredThat(response -> response.statusCode(400));
+//    }
+//
+//    @Step("I set GET method with valid endpoint and body requests Login SuperAdmin")
+//    public String setInvalidLoginSuperAdminReq2(){
+//        return url;
+//    }
+//
+//    @Step("I send Login requests to connect to APIs L6")
+//    public void sendLoginSuperAdminReq3(){
+//        SerenityRest.given().when().get(setInvalidLoginAdminReq2());
+//    }
+//
+//    @Step("I receive HTTP response code 400 for Invalid Request Login SuperAdmin")
+//    public void receiveLoginSuperAdminResp3(){
+//        restAssuredThat(response -> response.statusCode(400));
+//    }
 }
--- src/test/java/starter/stepdefinitions/LoginSteps.java
+++ src/test/java/starter/stepdefinitions/LoginSteps.java
@@ -25,35 +25,35 @@ public class LoginSteps {
         login.receiveLoginAdminResp1();
     }
 
-    @Given("I set POST method with valid endpoint and invalid body requests Login Admin")
-    public void setLoginAPIs2(){
-        login.setInvalidLoginAdminReq1();
-    }
-
-    @When("I send Login requests to connect to APIs L2")
-    public void sendLoginAPIs2(){
-        login.sendLoginAdminReq2();
-    }
-
-    @Then("I receive HTTP response code 400 for Invalid Login Admin")
-    public void receiveLoginAPIs2(){
-        login.receiveLoginAdminResp2();
-    }
-
-    @Given("I set GET method with valid endpoint and body requests Login Admin")
-    public void setLoginAPIs3(){
-        login.setInvalidLoginAdminReq2();
-    }
-
-    @When("I send Login requests to connect to APIs L3")
-    public void sendLoginAPIs3(){
-        login.sendLoginAdminReq3();
-    }
-
-    @Then("I receive HTTP response code 400 for Invalid Request Login Admin")
-    public void receiveLoginAPIs3(){
-        login.receiveLoginAdminResp3();
-    }
+//    @Given("I set POST method with valid endpoint and invalid body requests Login Admin")
+//    public void setLoginAPIs2(){
+//        login.setInvalidLoginAdminReq1();
+//    }
+//
+//    @When("I send Login requests to connect to APIs L2")
+//    public void sendLoginAPIs2(){
+//        login.sendLoginAdminReq2();
+//    }
+//
+//    @Then("I receive HTTP response code 400 for Invalid Login Admin")
+//    public void receiveLoginAPIs2(){
+//        login.receiveLoginAdminResp2();
+//    }
+//
+//    @Given("I set GET method with valid endpoint and body requests Login Admin")
+//    public void setLoginAPIs3(){
+//        login.setInvalidLoginAdminReq2();
+//    }
+//
+//    @When("I send Login requests to connect to APIs L3")
+//    public void sendLoginAPIs3(){
+//        login.sendLoginAdminReq3();
+//    }
+//
+//    @Then("I receive HTTP response code 400 for Invalid Request Login Admin")
+//    public void receiveLoginAPIs3(){
+//        login.receiveLoginAdminResp3();
+//    }
 
     @Given("I set POST method with valid endpoint and body requests Login SuperAdmin")
     public void setLoginAPIs4(){
@@ -70,33 +70,33 @@ public class LoginSteps {
         login.receiveLoginSuperAdminResp1();
     }
 
-    @Given("I set POST method with valid endpoint and invalid body requests Login SuperAdmin")
-    public void setLoginAPIs5(){
-        login.setInvalidLoginSuperAdminReq1();
-    }
-
-    @When("I send Login requests to connect to APIs L5")
-    public void sendLoginAPIs5(){
-        login.sendLoginSuperAdminReq2();
-    }
-
-    @Then("I receive HTTP response code 400 for Invalid Login SuperAdmin")
-    public void receiveLoginAPIs5(){
-        login.receiveLoginSuperAdminResp2();
-    }
-
-    @Given("I set GET method with valid endpoint and body requests Login SuperAdmin")
-    public void setLoginAPIs6(){
-        login.setInvalidLoginSuperAdminReq2();
-    }
-
-    @When("I send Login requests to connect to APIs L6")
-    public void sendLoginAPIs6(){
-        login.sendLoginSuperAdminReq3();
-    }
-
-    @Then("I receive HTTP response code 400 for Invalid Request Login SuperAdmin")
-    public void receiveLoginAPIs6(){
-        login.receiveLoginSuperAdminResp3();
-    }
+//    @Given("I set POST method with valid endpoint and invalid body requests Login SuperAdmin")
+//    public void setLoginAPIs5(){
+//        login.setInvalidLoginSuperAdminReq1();
+//    }
+//
+//    @When("I send Login requests to connect to APIs L5")
+//    public void sendLoginAPIs5(){
+//        login.sendLoginSuperAdminReq2();
+//    }
+//
+//    @Then("I receive HTTP response code 400 for Invalid Login SuperAdmin")
+//    public void receiveLoginAPIs5(){
+//        login.receiveLoginSuperAdminResp2();
+//    }
+//
+//    @Given("I set GET method with valid endpoint and body requests Login SuperAdmin")
+//    public void setLoginAPIs6(){
+//        login.setInvalidLoginSuperAdminReq2();
+//    }
+//
+//    @When("I send Login requests to connect to APIs L6")
+//    public void sendLoginAPIs6(){
+//        login.sendLoginSuperAdminReq3();
+//    }
+//
+//    @Then("I receive HTTP response code 400 for Invalid Request Login SuperAdmin")
+//    public void receiveLoginAPIs6(){
+//        login.receiveLoginSuperAdminResp3();
+//    }
 }
